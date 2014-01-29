0db2a251f152daa85e774cd218566c493d21d2b4
Luke <lukecathie@gmail.com>
2014-01-29 09:29:44 +0000

    Update config and remove db from git

--- temperature_log.rb
+++ temperature_log.rb
@@ -1,5 +1,5 @@
 require 'data_mapper' 
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/development.db")
+DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/#{ENV['env']}.db")
 
 class TemperatureLog
   include DataMapper::Resource
